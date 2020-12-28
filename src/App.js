87a8fa0d36374569a9a9714df655ba09fadedc59
jeromeski <jeromegee@gmail.com>
2020-12-28 08:18:57 +0800

    integrated routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './shared/Header';
 import { Container } from 'react-bootstrap';
 import RentalList from './components/rental/RentalList';
+import RentalDetail from './components/rental/RentalDetail';
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
       <Container>
-        <RentalList />
+        <Route exact path='/' component={RentalList} />
+        <Route exact path='/test' component={RentalDetail} />
       </Container>
-    </div>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/rental/RentalDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class RentalDetail extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>I am detail component</h1>      
+      </div>
+    );
+  }
+}
+
+export default RentalDetail;
