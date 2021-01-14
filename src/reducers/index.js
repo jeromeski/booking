f4b8c040301c823d03aa321f276e2a01eebfd2b2
jeromeski <jeromegee@gmail.com>
2021-01-15 07:49:20 +0800

    Using Redux Dev Tools

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { rentalReducer } from './rental-reducer';
 import { selectedRentalReducer } from './selectedRental-reducer';
@@ -8,6 +8,10 @@ export const init = () => {
     rentals: rentalReducer,
     rental: selectedRentalReducer
   });
-  const store = createStore(reducer, applyMiddleware(thunk));
+
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
   return store;
 };
