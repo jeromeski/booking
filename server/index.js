42c9843ff817904c4053193c149ea75da01fbbed
jeromeski <jeromegee@gmail.com>
2021-01-15 16:04:39 +0800

    Testing first endpoint with postman

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/rentals', (req, res) => {
+  res.json({
+    hello: 'World'
+  });
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
