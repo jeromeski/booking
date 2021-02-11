05cdd987a5f4cec6c562198f2f64f001690c1b15
jeromeski <jeromegee@gmail.com>
2021-02-11 18:47:38 +0800

    Handle map error

--- src/components/map/GoogleMap.js
+++ src/components/map/GoogleMap.js
@@ -4,17 +4,29 @@ import {
   withGoogleMap,
   GoogleMap,
   Circle,
+  InfoWindow,
 } from 'react-google-maps';
 import Cacher from '../../services/cacher';
 
 const MapComponent = (props) => {
-  const { coordinates } = props;
+  const { coordinates, isError, isLocationLoaded } = props;
   return (
     <GoogleMap
       defaultZoom={13}
       defaultCenter={coordinates}
       center={coordinates}>
-      <Circle center={coordinates} radius={500} />
+      {isLocationLoaded && !isError && (
+        <Circle center={coordinates} radius={500} />
+      )}
+      {isLocationLoaded && isError && (
+        <InfoWindow position={coordinates} options={{ maxWidth: 300 }}>
+          <div>
+            Ooops, something went wrong... Please send a message in the contact
+            page for additional information regarding this place. We are sorry
+            for the inconvinience.
+          </div>
+        </InfoWindow>
+      )}
     </GoogleMap>
   );
 };
@@ -22,50 +34,53 @@ const MapComponent = (props) => {
 const withGeoCode = (WrappedComponent) => {
   
   return class extends React.Component {
-
     cacher = new Cacher();
     state = {
       coordinates: {
         lat: 0,
         lng: 0,
       },
+      isError: false,
+      isLocationLoaded: false
     };
 
     componentDidMount() {
       this.getGeocodedLocation();
     }
 
-    geocodeLocation() {
-      const { location } = this.props;
+    geocodeLocation(location) {      
       const geoCoder = new window.google.maps.Geocoder();
 
-      return new Promise ((resolve, reject) => {
+      return new Promise((resolve, reject) => {
         geoCoder.geocode({ address: location }, (result, status) => {
           if (status === 'OK') {
             const geometry = result[0].geometry.location;
             const coordinates = { lat: geometry.lat(), lng: geometry.lng() };
             this.cacher.handleCacheValue(location, coordinates);
-            resolve(coordinates)
+            resolve(coordinates);
           } else {
-            reject('ERROR!!!')
+            reject('ERROR!!!');
           }
         });
-      })
+      });
     }
 
     getGeocodedLocation() {
       const location = this.props.location;
-      if  (this.cacher.isValueCached(location)) {
-        this.setState({  coordinates: this.cacher.getChachedValue(location)  });;
+      if (this.cacher.isValueCached(location)) {
+        this.setState({ coordinates: this.cacher.getChachedValue(location), isLocationLoaded: true });
       } else {
         this.geocodeLocation(location)
-          .then(
-            (coordinates) => {
-              this.setState({coordinates});
-            },
-            (error) => {
-              console.log(error)
-            });
+        .then(
+          (coordinates) => {
+            this.setState({ coordinates, isLocationLoaded: false });
+          },
+          (error) => {
+            this.setState({
+              isError: true, isLocationLoaded: true 
+             });
+          }
+        );
       }
     }
     render() {
