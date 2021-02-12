dab1ddda64a5085bd0342261af33340349f597c6
jeromeski <jeromegee@gmail.com>
2021-02-12 08:54:02 +0800

    Starting Register

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,10 +1,66 @@
+const User = require('../models/user');
+
 exports.auth = (req, res) => {};
 
 exports.register = (req, res) => {
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = req.body.password;
-  const password2 = req.body.passsword2;
+  const { username, email, password, password2 } = req.body;
+  if (!password || !email) {
+    return res.status(422).send({
+      errors: [
+        { title: 'Data missing', detail: 'Provide email and passsword!' },
+      ],
+    });
+  }
 
-  res.json({ username, email });
+  if (password !== password2) {
+    return res.status(422).send({
+      errors: [
+        {
+          title: 'Invalid Password',
+          detail: 'Password is not the same as confirmation',
+        },
+      ],
+    });
+  }
+
+  User.findOne({ email }, (err, existingUser) => {
+    if (err) {
+      return  res.status(422).send({
+        errors: [
+          {
+            title: 'Some Error',
+            detail: 'Will be tackled later',
+          },
+        ],
+      });
+    }
+    if (existingUser) {
+      return res.status(422).send({
+        errors: [
+          {
+            title: 'User Exists',
+            detail: 'This email already exists in the database!',
+          },
+        ],
+      });
+    }
+
+    const user = new User({
+      username,
+
+           email,
+     
+      password,
+    });
+
+    user.save((err) => {
+      if(err) {
+        return res.status(422).send({
+          'mongoose': 'Handle mongoose error next lecture'
+        });
+      } else {
+        return res.send({'registered': true})
+      }
+    });
+  });
 };
--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-useless-escape */
 const mongoose = require('mongoose');
-const Schema = new mongoose.Schema();
+const Schema = mongoose.Schema;
 
-const userSchema = Schema({
+const userSchema = new Schema({
   username: {
     type: String,
     min: [4, 'Too short, min is 4 characters'],
-    max: [32, 'Too long max is 32 characters'],
+    max: [32, 'Too long max is 32 characters']
   },
   email: {
     type: String,
@@ -15,15 +15,15 @@ const userSchema = Schema({
     unique: true,
     lowercase: true,
     required: 'Email is required',
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
   },
   password: {
     type: String,
     min: [4, 'Too short, min is 4 characters'],
     max: [32, 'Too long, max is 32 characters'],
-    required: 'Password is required',
+    required: 'Password is required'
   },
-  rentals: [{ type: Schema.Types.ObjectId, ref: 'Rental' }],
+  rentals: [{ type: Schema.Types.ObjectId, ref: 'Rental' }]
 });
 
 module.exports = mongoose.model('User', userSchema);
