97542d84314d8333f769e64733c544418b58ef5a
jeromeski <jeromegee@gmail.com>
2021-02-14 18:07:58 +0800

    Register Form Validation

--- src/components/register/RegisterForm.js
+++ src/components/register/RegisterForm.js
@@ -1,82 +1,93 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+const renderField = ({
+  input,
+  label,
+  type,
+  className,
+  meta: { touched, error, warning }
+}) => (
+  <div className='form-group'>
+    <label>{label}</label>
+    <div className='input-group'>
+      <input {...input} type={type} className={className} />
+    </div>
+    {touched && error && <div className='alert alert-danger'>{error}</div>}
+  </div>
+);
+
 const RegisterForm = (props) => {
   const { handleSubmit, pristine, submitting, submitCb } = props;
   return (
     <form onSubmit={handleSubmit(submitCb)}>
-      <div>
-        <label>First Name</label>
-        <div>
-          <Field
-            name='firstName'
-            component='input'
-            type='text'
-            placeholder='First Name'
-            className='form-control'
-          />
-        </div>
-      </div>
-      <div>
-        <label>Last Name</label>
-        <div>
-          <Field
-            name='lastName'
-            component='input'
-            type='text'
-            placeholder='Last Name'
-            className='form-control'
-          />
-        </div>
-      </div>
-      <div>
-        <label>Email</label>
-        <div>
-          <Field
-            name='email'
-            component='input'
-            type='email'
-            placeholder='Email'
-            className='form-control'
-          />
-        </div>
-      </div>
-      <div>
-        <label>Password</label>
-        <div>
-          <Field
-            name='password'
-            component='input'
-            type='password'
-            placeholder='Password'
-            className='form-control'
-          />
-        </div>
-      </div>
-      <div>
-        <label>Confirm Password</label>
-        <div>
-          <Field
-            name='password2'
-            component='input'
-            type='password'
-            placeholder='Confirm Password'
-            className='form-control'
-          />
-        </div>
-      </div>
-      <div>
-        <button
-          className='btn btn-bwm btn-form'
-          type='submit'
-          disabled={pristine || submitting}>
-          Submit
-        </button>
-      </div>
+      <Field
+        name='firstName'
+        type='text'
+        className='form-control'
+        component={renderField}
+        label='Username'
+      />
+      <Field
+        name='lastName'
+        type='text'
+        className='form-control'
+        component={renderField}
+        label='Lastname'
+      />
+      <Field
+        name='email'
+        type='email'
+        className='form-control'
+        component={renderField}
+        label='email'
+      />
+      <Field
+        name='password'
+        type='password'
+        className='form-control'
+        component={renderField}
+        label='Password'
+      />
+      <Field
+        name='password2'
+        type='password'
+        className='form-control'
+        component={renderField}
+        label='Confirm Password'
+      />
+      <button
+        className='btn btn-bwm btn-form'
+        type='submit'
+        disabled={pristine || submitting}>
+        Submit
+      </button>
     </form>
   );
 };
 
+const validate = (values) => {
+  const errors = {};
+
+  if (values.username && values.username.length < 4) {
+    errors.username = 'Username must have at least 4 characters';
+  }
+
+  if (!values.email) {
+    errors.email = 'Please enter email!';
+  }
+
+  if (!values.password2) {
+    errors.email = 'Please enter to confirm your password';
+  }
+
+  if (values.password !== values.password2) {
+    errors.password = 'Passwords must be the same';
+  }
+  return errors;
+};
+
 export default reduxForm({
-  form: 'registerForm'
+  form: 'registerForm',
+  validate
 })(RegisterForm);
