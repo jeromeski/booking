2d803b6ba43fbb973393b33077eac40526621ab3
jeromeski <jeromegee@gmail.com>
2021-02-17 11:06:31 +0800

    Add booking model

--- server/models/user.js
+++ server/models/user.js
@@ -5,27 +5,28 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    min: [4, 'Too short, min is 4 characters'],
-    max: [32, 'Too long max is 32 characters']
-  },
-  email: {
-    type: String,
-    min: [4, 'Too short, min is 4 characters'],
-    max: [32, 'Too long, max is 32 characters'],
-    unique: true,
-    lowercase: true,
-    required: 'Email is required',
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
-  },
-  password: {
-    type: String,
-    min: [4, 'Too short, min is 4 characters'],
-    max: [32, 'Too long, max is 32 characters'],
-    required: 'Password is required'
-  },
-  rentals: [{ type: Schema.Types.ObjectId, ref: 'Rental' }]
+	username: {
+		type: String,
+		min: [4, 'Too short, min is 4 characters'],
+		max: [32, 'Too long max is 32 characters']
+	},
+	email: {
+		type: String,
+		min: [4, 'Too short, min is 4 characters'],
+		max: [32, 'Too long, max is 32 characters'],
+		unique: true,
+		lowercase: true,
+		required: 'Email is required',
+		match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+	},
+	password: {
+		type: String,
+		min: [4, 'Too short, min is 4 characters'],
+		max: [32, 'Too long, max is 32 characters'],
+		required: 'Password is required'
+	},
+	rentals: [{ type: Schema.Types.ObjectId, ref: 'Rental' }],
+	bookings: [{ type: Schema.Types.ObjectId, ref: 'Booking' }]
 });
 
 userSchema.methods.hasSamePassword = function (requestedPassword) {
