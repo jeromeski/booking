b7abb566f7bec5d0e861400e0cf337a6fa4ac9e0
jeromeski <jeromegee@gmail.com>
2021-02-19 04:47:54 +0800

    Booking Layout

--- src/components/rental/rental-detail/RentalDetail.js
+++ src/components/rental/rental-detail/RentalDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchRentalById } from '../../../actions';
+import Booking from '../../booking/Booking';
 import RentalDetailInfo from './RentalDetailInfo';
 import RentalMap from './RentalMap';
 
@@ -15,29 +16,31 @@ class RentalDetail extends Component {
 
     if (rental._id) {
       return (
-        <section id='rentalDetails'>
-          <div className='upper-section'>
-            <div className='row'>
-              <div className='col-md-6'>
-                <img src={rental.image} alt=''></img>
-              </div>
-              <div className='col-md-6'>
-                <RentalMap location={`${rental.city}, ${rental.street}`} />
-              </div>
-            </div>
-          </div>
+				<section id='rentalDetails'>
+					<div className='upper-section'>
+						<div className='row'>
+							<div className='col-md-6'>
+								<img src={rental.image} alt=''></img>
+							</div>
+							<div className='col-md-6'>
+								<RentalMap location={`${rental.city}, ${rental.street}`} />
+							</div>
+						</div>
+					</div>
 
-          <div className='details-section'>
-            <div className='row'>
-              <div className='col-md-8'>
-                <RentalDetailInfo rental={rental} />
-              </div>
+					<div className='details-section'>
+						<div className='row'>
+							<div className='col-md-8'>
+								<RentalDetailInfo rental={rental} />
+							</div>
 
-              <div className='col-md-4'>BOOKING</div>
-            </div>
-          </div>
-        </section>
-      );
+							<div className='col-md-4'>
+								<Booking rental={rental} />
+							</div>
+						</div>
+					</div>
+				</section>
+			);
     } else {
       return <h1>Loading...</h1>;
     }
